bb1000033cd1b8e3ee54fee3cf5a23aea285b1ab
TudorFCH <tudorpopa313@gmail.com>
2024-01-11 09:38:40 +0200

    Lab5

--- Lab5/repository/OrderRepo.py
+++ Lab5/repository/OrderRepo.py
@@ -76,48 +76,6 @@ class OrderRepo(DataRepo):
         pickle.dump(string, f)
         f.close()
 
-    def calc_cost(self, dish_repo: CookedDishRepo, drink_repo: DrinkRepo):
-        def calculate_total_cost_food(total, item):
-            dish = dish_repo.get_cookedDishID(item)
-            if dish:
-                return total + dish.price
-            return total
-
-        total_dish_cost = reduce(calculate_total_cost_food, self.list_of_dish_id, 0)
-
-        def calculate_total_cost_drink(total, item):
-            drink = drink_repo.get_drink_id(item)
-            if drink:
-                return total + drink.price
-            return total
-
-        total_drink_cost = reduce(calculate_total_cost_drink, self.list_of_drink_id, 0)
-
-        total_cost = total_dish_cost + total_drink_cost
-
-        return (
-            f'Total Dish Cost = {total_dish_cost} \nTotal Drink Cost = {total_drink_cost} \nTotal Order Cost = {total_cost}')
-
-    def bill(self, dish_repo: CookedDishRepo, drink_repo: DrinkRepo):
-        dishes = {}
-        drinks = {}
-
-        for dish_id in self.list_of_dish_id:
-            dish = dish_repo.get_cookedDishID(dish_id)
-            if dish:
-                dishes[dish.id] = dish.price
-
-        for drink_id in self.list_of_drink_id:
-            drink = drink_repo.get_drink_id(drink_id)
-            if drink:
-                drinks[drink.id] = drink.price
-
-        return dishes, drinks
-
-    def show_bill(self, dish_repo, drink_repo):
-        result1, result2 = self.bill(dish_repo, drink_repo)
-        return f'The List of Orderd Dishes: {result1} \n The List of Orderd Drinks: {result2}'
-
 
     def convert_to_string(self):
         pass
